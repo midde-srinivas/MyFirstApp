ca140c60651ce3201082aa75ec103131ca15aade
SRINIVAS <SRINIVAS@192.168.0.102>
2016-05-03 00:20:36 +0530

    Commit 4

--- MyFirstApp/src/MainClass.java
+++ MyFirstApp/src/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("My First Change to this file");
+		System.out.println("My Second Change to this file");
 	}
 
 }
